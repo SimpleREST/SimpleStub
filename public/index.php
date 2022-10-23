35872a4962bb0571562fb79662c2352602081ecb
Dmitriy <stdimensiy@yandex.ru>
2022-10-24 00:30:53 +0300

    Формирование базовой файловой структуры проекта, оформление.

--- public/index.php
+++ public/index.php
@@ -1,2 +1,16 @@
-<?php
-echo "Hello!!!";
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>Simple stub - The site is in the process of being created.</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+</head>
+<body>
+<p>Разум — это Будда, а прекращение умозрительного мышления — это путь.
+    Перестав мыслить понятиями и размышлять о путях существования и небытия,
+    о душе и плоти, о пассивном и активном и о других подобных вещах,
+    начинаешь осознавать, что разум — это Будда,
+    что Будда — это сущность разума,
+    и что разум подобен бесконечности.</p>
+</body>
+</html>
