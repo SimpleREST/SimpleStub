d4b38ebcb97653afad4da2f71e7c48e893080751
Dmitriy <stdimensiy@yandex.ru>
2022-10-25 23:24:14 +0300

    Расширение конфигурационных данных заглушки, зачистка index.php

--- config/config.php
+++ config/config.php
@@ -8,6 +8,47 @@
  * ----------------------------------------------------------------------------------------*/
 
 return [
-    'base_lang' => 'en',
+    'base_lang' => 'ru',
     'base_charset' => 'utf-8',
+
+    /**
+     * Секция управления блоком обратного отсчета
+     */
+
+    'countdown_date' => '2022/12/30',
+    'countdown_note' => 'до старта сайта <BR> %w недель %d дней <br> %H:%M:%S',
+
+    /**
+     * Секция управления блоком основного контента заглушки
+     */
+    'base_title' => 'Website in development',
+    'base_note' => 'The service is under development. Soon we will open it to everyone.',
+
+    /**
+     * Секция управления блоком отображения контактной информации заглушки
+     */
+    'contact-address' => 'Россия, Санкт-Петербург. Проспект Энтузиастов д.30',
+    'contact_phone_to_script' => '+79119411234',
+    'contact-phone_to_display' => '+7 (911) 941 -- --',
+    'contact-email_to_script' => 'info@simplerest.ru',
+    'contact-email_to_display' => 'info@simplerest.ru',
+
+    /**
+     * Секция управления блоком обратной связи
+     */
+    'form-name-minimum-length' => 4,
+    'form-name-placeholder' => 'Ваше Имя',
+    'form-name-minimum-length-error-msg' => 'Пожалуйста укажите правильный адрес электронной почты',
+    'form-email-placeholder' => 'Ваш Email',
+    'form-email-validate-error-msg' => 'Пожалуйста укажите правильный адрес электронной почты',
+    'form-subject-minimum-length' => 4,
+    'form-subject-placeholder' => 'Тема или предмет сообщения',
+    'form-subject-minimum-length-error-msg' => 'Пожалуйста определите тему своего обращения',
+    'form-message-rows-count' => 5,
+    'form-message-placeholder' => 'Текст сообщения',
+    'form-message-minimum-length-error-msg' => 'Пожалуйста напишите что-нибудь в этом поле',
+    'form-action-msg-loading' => 'Загрузка',
+    'form-action-msg-success' => 'Ваше сообщение отправлено. Спасибо!',
+    'form-action-msg-error' => 'Ошибка! Ваше сообщение НЕ отправлено. Повторите ввод корректных данных',
+    'form-button-submit-text' => 'Отправить сообщение',
 ];
--- public/index.php
+++ public/index.php
@@ -7,19 +7,14 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = new Stub();
 ?>
 <!DOCTYPE html>
-<html lang="ru">
+<html lang="<?php echo $app->get('base_lang', 'en') ?>">
 <head>
     <title>Simple stub - The site is in the process of being created.</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta charset="utf-8">
+    <meta charset="<?php echo $app->get('base_charset', 'utf-8') ?>">
 </head>
 <body>
 <p><?php echo $app->version() ?></p>
-<p><?php echo $app->get('base_lang') ?></p>
-<p><?php echo $app->get('base_lang', 'RU') ?></p>
-<p><?php echo $app->get('base_lang1', 'RU') ?></p>
-<p><?php echo Stub::VERSION ?></p>
-<p><?php $app->printOutVersion(); ?></p>
 <p>Разум — это Будда, а прекращение умозрительного мышления — это путь.
     Перестав мыслить понятиями и размышлять о путях существования и небытия,
     о душе и плоти, о пассивном и активном и о других подобных вещах,
