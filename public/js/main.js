dadb36cadd7fc1d4134d6543e8c6be9972f970bf
Dmitriy <stdimensiy@yandex.ru>
2022-11-07 01:06:04 +0300

    Разработка таймера обратного отсчета. Зачистка кода таймера

--- public/js/main.js
+++ public/js/main.js
@@ -4,15 +4,17 @@
  * License: https://github.com/SimpleREST/SimpleStub/blob/master/LICENSE
  *
  * содержит только уникальные скрипты предназначенные для конкретной заглушки (нет смысла в отдельную
- * зависимость запихивать... пока.
+ * зависимость запихивать... пока).
  */
 (function () {
     'use strict';
     let y = null;
+    let m = null;
+    let d = null;
     let deadline = null;
     let diff = null;
     let timerId = null;
-    let pattern = null;
+    let pattern = "";
 
     function $(x) {
         const elem = document.querySelectorAll(x);
@@ -35,8 +37,8 @@
              */
             const count_array = count.split('/');
             y = Number(count_array[0].trim());
-            var m = Number(count_array[1].trim());
-            var d = Number(count_array[2].trim());
+            m = Number(count_array[1].trim());
+            d = Number(count_array[2].trim());
         }
         pattern = a.dataset.pattern;
         if (!pattern) pattern = "Шаблон вывода значения таймера не задан"
@@ -45,13 +47,24 @@
         console.log("Сработала функция (основное тело)" + a)
     }
 
-    function generate_a_result_string(diff, pattern) {
-        let result_string = "";
+    /**
+     * Функция генерирует результирующую строку из передаваемого в качестве аргумента
+     * шаблона и временной разницы
+     * @param diff - разниц во времени в миллисекундах
+     * @param pattern - созданный по определенным правилам шаблон строки результата
+     * @return String
+     */
+    function generate_a_result_string(diff = 0, pattern = "") {
+        let result_string;
         const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
         const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
         const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
         const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
-        result_string = pattern.replace('%d', days).replace('%H', hours).replace('%M', minutes).replace('%S', seconds);
+        result_string = pattern
+            .replace('%d', days.toString())
+            .replace('%H', hours.toString())
+            .replace('%M', minutes.toString())
+            .replace('%S', seconds.toString());
         return result_string;
     }
 
